d517f156062d55902813eeaa85c7c41ecd5106e7
Argifari <argifari40@gmail.com>
2024-10-09 19:23:49 +0700

    summit

--- DASPRO KULIAH/fun.py
+++ DASPRO KULIAH/fun.py
@@ -518,13 +518,7 @@ def IsTegakLurus(L1,L2):
 
 print(Gradien(MakeGaris(MakePoint(1,2), MakePoint(2,1))))
 print(IsSejajar(MakeGaris(MakePoint(1,2), MakePoint(3,2)), MakeGaris(MakePoint(1,2),MakePoint(2,2))))
-
 print(IsTegakLurus(MakeGaris(MakePoint(0,0), MakePoint(5,0)), MakeGaris(MakePoint(1,0),MakePoint(1,-10))))
-
-
-
-
-
 
 
 
