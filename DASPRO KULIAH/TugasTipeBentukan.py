9e9901150eb39984bff23201de938f9d0d657a89
Argifari <argifari40@gmail.com>
2024-10-14 16:10:31 +0700

    COMMIT

--- DASPRO KULIAH/TugasTipeBentukan.py
+++ DASPRO KULIAH/TugasTipeBentukan.py
@@ -3,6 +3,55 @@
 # =================================================
 
 
+# DEFINISI DAN SPESIFIKASI TYPE
+# tpye pecahanCampuran : <bil: integer, n:integer >= 0, d:integer > 0 >
+#   {<bil,n,d> adalah sebuah pecahan campuran dengan bil dapat bernilai positif, negatif, maupun nol, 
+#       n selalu bernilai lebih kecil dari d, dengan d lebih dari nol.}
+# type pecahan : <n:integer, d: integer > 0>
+#   {<n,d> adalah pecahan biasa dengan n bisa bernilai negatif, nol, maupun positif, dan d lebih besar dari nol.}
+#
+# DEFINISI DAN SPESIFIKASI SELEKTOR
+# Bil : pecahanCampuran --> integer
+#   {Bil(P) memberikan bilangan bil dari pecahan campuran P, <bil,n,d>.}
+# PembPecahanCampuran : pecahanCampuran --> integer >= 0
+#   {PembPecahanCampuran(P) memberikan pembilang n dari pecahan campuran P, <bil,n,d>.}
+# PenyPecahanCampuran : pecahanCampuran --> integer > 0
+#   {PenyPecahanCampuran(P) memberikan penyebut d dari pecahan campuran P, <bil,n,d>.}
+# PembPecahan : pecahan --> integer 
+#   {PembPecahan(PB) memberikan pembilang n dari pecahan PB, <n,d>.}
+# PenyPecahan : pecahan --> integer > 0
+#   {PenyPecahan(PB) memberikan penyebut d dari pecahan PB, <n,d,>.}
+#
+# DEFINISI DAN SPESIFIKASI KONSTRUKTOR
+# MakePecahanCampuran : integer, integer >= 0, integer > 0 --> pecahanCampuran
+#   {MakePecahanCampuran(bil,n,d) membentuk pecahan campuran, <bil,n,d>.}
+# MakePecahan : integer, integer > 0 --> pecahan
+#   {MakePecahan(n,d) membentuk pecahan, <n,d>.}
+#
+# DEFINISI DAN SPESIFIKASI FUNGSI/OPERATOR TERHADAP PECAHAN CAMPURAN
+# KonversiPecahan : pecahanCampuran --> pecahan
+#   {KonversiPecahan(P) mengkonversi sebuah pecahan campuran menjadi pecahan.}
+# KonversiReal : pecahanCampuran --> real
+#   {KonversiReal(P) mengkonversi sebuah pecahan campuran menjadi bilangan real.}
+# AddP : 2 pecahanCampuran --> pecahanCampuran
+#   {AddP(P1,P2) menambahkan pecahan campuran P1 dengan pecahan campuran P2.}
+# SubP : 2 pecahanCampuran --> pecahanCampuran
+#   {SubP(P1,P2) mengurangi pecahan campuran P1 dengan pecahan campuran P2.}
+# DivP : 2 pecahanCampuran --> pecahan
+#   {DivP(P1,P2) membagi pecahan campuran P1 dengan pecahan campuran P2.}
+# MulP : 2 pecahanCampuran --> Pecahan
+#   {MulP(P1,P2) mengalikan pecachan campuran P1 dengan pecahan campuran P2}
+#
+# DEFINISI DAN SPESIFIKASI PREDIKAT
+# IsEqP? : 2 pecahanCampuran --> boolean
+#   {IsEqP?(P1,P2) bernilai benar jika pecahan campuran P1 sama dengan pecahan campuran P2.}
+# IsLtP? : 2 pecahanCampuran --> boolean
+#   {IsLtP?(P1,P2) bernilai benar jika pecahan campuran P1 kurang dari pecahan campuran P2.}
+# IsGtP? : 2 pecahanCampuran --> boolean
+#   {IsGtP?(P1,P2) bernilai benar jika pecahan campuran P1 lebih dari pecahan campuran P2.}
+#
+# REALISASI DALAM PYTHON
+
 def Bil(P) :
     return P[0]
 
@@ -12,11 +61,11 @@ def PembPecahanCampuran(P) :
 def PenyPecahanCampuran(P) :
     return P[2]
 
-def PembPecahan(P) :
-    return P[0]
+def PembPecahan(PB) :
+    return PB[0]
 
-def PenyPecahan(P):
-    return P[1]
+def PenyPecahan(PB):
+    return PB[1]
 
 def MakePecahanCampuran(bil,n,d) :
     return [bil,n,d]
@@ -68,25 +117,61 @@ def MulP(P1,P2) :
             PenyPecahan(KonversiPecahan(P1))*PenyPecahan(KonversiPecahan(P2))
         )
 
-def EqP(P1,P2) :
+def IsEqP(P1,P2) :
     return (PembPecahan(KonversiPecahan(P1))*PenyPecahan(KonversiPecahan(P2)) 
             == PembPecahan(KonversiPecahan(P2))*PenyPecahan(KonversiPecahan(P1)))
 
-def LtP(P1,P2) :
+def IsLtP(P1,P2) :
     return (PembPecahan(KonversiPecahan(P1))*PenyPecahan(KonversiPecahan(P2)) 
             < PembPecahan(KonversiPecahan(P2))*PenyPecahan(KonversiPecahan(P1)))
 
-def GtP(P1,P2) :
+def IsGtP(P1,P2) :
     return (PembPecahan(KonversiPecahan(P1))*PenyPecahan(KonversiPecahan(P2)) 
             > PembPecahan(KonversiPecahan(P2))*PenyPecahan(KonversiPecahan(P1))) 
 
-#print(EqP(MakePecahanCampuran(2,3,4), MakePecahanCampuran(2,3,4)))
+# APLIKASI DALAM PYTHON
+print(IsEqP(MakePecahanCampuran(2,3,4), MakePecahanCampuran(2,3,4)))
 
 
 # NO 2 TIPE BENTUKAN GARIS
 # ==============================================
 # ==============================================
 
+# DEFINISI DAN SPESIFIKASI TYPE
+# type point : <x:real, y:real>
+#   {<x,y> adalah sebuah point dengan x sebagai absis dan y sebagai ordinat.}
+# type garis : <P1:point, P2:point> 
+#   {<P1,P2> adalah sebuah garis dengan 2 point.}
+#
+# DEFINISI DAN SPESIFIKASI SELEKTOR
+# Absis : point --> real
+#   {Absis(P) memberikan nilai absis x dari point P, <x,y>.}
+# Ordinat : point --> real
+#   {Ordinat(P) memberikan nilai ordinat y dari point P, <x,y>.}
+# GarisPoint1 : garis --> point
+#   {GarisPoint1(G) memberikan point P1 dari garis G, <P1,P2>.}
+# GarisPoint2 : garis --> point
+#   {GarisPoint2(G) memberikan point P2 dari garis G, <P1,P2>.}
+#
+# DEFINISI DAN SPESIFIKASI KONSTRUKTOR
+# MakePoint : 2 real --> point
+#   {MakePoint(x,y) membentuk sebuah point.}
+# MakeGaris : 2 point --> garis
+#   {MakeGaris(P1,P2) membentuk sebuah garis.}
+#
+# DEFINISI DAN SPESIFIKASI FUNGSI/OPERATOR TERHADAP GARIS
+# Gradien : garis --> real
+#   {Gradien(G) menghitung gradien dari sebuah garis.}
+# PanjangGaris : garis --> real
+#   {PanjangGaris(G) menghitung panjang sebuah garis.}
+#
+# DEFINISI DAN SPESIFIKASI PREDIKAT
+# IsSejajar? : 2 garis --> boolean
+#   {IsSejajar?(G1,G2) bernilai benar jika kedua gradien bernilai sama.}
+# IsTegakLurus? : 2 garis --> boolean
+#   {IsTegakLurus?(G1,G2) bernilai benar jika perkalian kedua gradien sama dengan -1.}
+#
+# REALISASI DALAM PYTHON
 
 def MakePoint(x,y):
     return [x,y]
@@ -100,49 +185,50 @@ def Ordinat(P):
 def MakeGaris(P1,P2) :
     return [P1,P2]
 
-def GarisPoint1(L) :
-    return L[0]
+def GarisPoint1(G) :
+    return G[0]
 
-def GarisPoint2(L) :
-    return L[1]
+def GarisPoint2(G) :
+    return G[1]
 
-def Gradien(L) :
-    if (Ordinat(GarisPoint1(L)) - Ordinat(GarisPoint2(L)) == 0) :
-        if (Absis(GarisPoint1(L)) - Absis(GarisPoint2(L)) == 0) :
+def Gradien(G) :
+    if (Ordinat(GarisPoint1(G)) - Ordinat(GarisPoint2(G)) == 0) :
+        if (Absis(GarisPoint1(G)) - Absis(GarisPoint2(G)) == 0) :
             return float('inf')
         else :
             return 0
     
-    elif (Absis(GarisPoint1(L)) - Absis(GarisPoint2(L)) == 0) :
+    elif (Absis(GarisPoint1(G)) - Absis(GarisPoint2(G)) == 0) :
         return float('inf')
     
     else :
-        return (    (Ordinat(GarisPoint1(L)) - Ordinat(GarisPoint2(L))) 
+        return (    (Ordinat(GarisPoint1(G)) - Ordinat(GarisPoint2(G))) 
                                 / 
-                    (Absis(GarisPoint1(L)) - Absis(GarisPoint2(L)))
+                    (Absis(GarisPoint1(G)) - Absis(GarisPoint2(G)))
                 )
 
 def PanjangGaris(P1,P2) :
     return ((Absis(P1) - Absis(P2))**2 + (Ordinat(P1) - Ordinat(P2))**2)**0.5
 
-def IsSejajar(L1,L2) :
-    return Gradien(L1) == Gradien(L2)
+def IsSejajar(G1,G2) :
+    return Gradien(G1) == Gradien(G2)
 
-def IsTegakLurus(L1,L2):
-    if Gradien(L1) == float('inf') :
-        if Gradien(L2) == 0 :
+def IsTegakLurus(G1,G2):
+    if Gradien(G1) == float('inf') :
+        if Gradien(G2) == 0 :
             return True
         else :
             return False
-    elif Gradien(L2) == float('inf') :
-        if Gradien(L1) == 0:
+    elif Gradien(G2) == float('inf') :
+        if Gradien(G1) == 0:
             return True
         else :
             return False
     else :
-        return (Gradien(L1) * Gradien(L2) == -1)
+        return (Gradien(G1) * Gradien(G2) == -1)
         
 
+# APLIKASI DALAM PYTHON
 
 print(Gradien(MakeGaris(MakePoint(1,2), MakePoint(2,1))))
 print(IsSejajar(MakeGaris(MakePoint(1,2), MakePoint(3,2)), MakeGaris(MakePoint(1,2),MakePoint(2,2))))
