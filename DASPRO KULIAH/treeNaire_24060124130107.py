f35cb0c0aea1a0dd3a959a416bcc5788bdd8b651
Argifari <argifari40@gmail.com>
2024-11-27 13:44:37 +0700

    sdfsdfsd

--- DASPRO KULIAH/treeNaire_24060124130107.py
+++ DASPRO KULIAH/treeNaire_24060124130107.py
@@ -14,16 +14,24 @@ def makePN(A,PN) :
 # DEFINISI DAN SPESIFIKASI SELEKTOR
 # akar : PohonN-ner tidak kosong --> elemen
 # {akar(PN) adalah akar dari PN. Jika PN adalah (A,P) = akar(PN) adalah A.}
-
+ 
 def akar(PN):
     return PN[0]
 
-# anak : PohonN-ner tidak kosong --> elemen
+# anak : PohonN-ner tidak kosong --> list of PohonN-ner
 # {anak(PN) adalah list of pohon N-ner yang merupakan anak-anak (sub pohon) 
 # dari PN. Jika PN adalah (A, P) maka anak PN adalah P}
 
+# REALISASI
+
 def anak(PN):
     return PN[1]
+
+# saudara : PohonN-ner --> list of PohonN-ner
+# {saudara(PN) mengembalikan list of pohon N-ner yang merupakan saudara atau yang 
+#  selevel.}
+
+# REALISASI
 
 def saudara(PN):
     return PN[1:]
@@ -32,18 +40,24 @@ def saudara(PN):
 # IsTreeEmpty : PohonN-ner --> boolean
 # {IsTreeEmpty(PN) bernilai benar jika PN kosong.}
 
+# REALISASI
+
 def IsTreeEmpty(PN):
     return PN == []
 
 # IsOneElmtTree : PohonN-ner --> boolean
-# {IsOneElmt(PN) bernilai benar jika PN hanya terdiri dari akar.}
+# {IsOneElmtTree(PN) bernilai benar jika PN hanya terdiri dari akar.}
+
+# REALISASI
 
 def IsOneELmtTree(PN):
     return (not IsTreeEmpty(PN)) and (IsTreeEmpty(anak(PN)))
 
 # DEFINISI DAN SPESIFIKASI FUNGSI OPERATOR TERHADAP POHONN-NER
-# NbElmtTree : PohonN-ner --> integer >= 0 
-# {NbElmtTree(PN) memberikan banyaknya node dari pohon PN.}
+# NbNElmtTree : PohonN-ner --> integer >= 0 
+# {NbNElmtTree(PN) memberikan banyaknya node dari pohon PN.}
+
+# REALISASI
 
 def NbNElmtTree(PN):
     if IsTreeEmpty(PN):
@@ -53,18 +67,23 @@ def NbNElmtTree(PN):
     else :
         return 1 + NbNElmtTree(akar(anak(PN))) + NbNElmtChild(saudara(anak(PN)))
 
-T2 = makePN('A',[makePN('B',[makePN('D',[]),makePN('E',[]),makePN('F',[])]),
-                 makePN('C',[makePN('G',[]),makePN('H',[makePN('I',[])])])])
+# NbNElmtChild : PohonN-ner --> integer >= 0
+# {NbNElmtChild(PN) menghitung jumlah elemen pada sisa anak PN.}
 
-# NbElmtChild : PohonN-ner --> integer >= 0
-# {NbElmtChild(PN)  }
+# REALISASI
+
 def NbNElmtChild(PN): 
     if IsTreeEmpty(PN):
         return 0
     else:
-        return NbNElmtTree(akar(PN)) + NbNElmtChild(saudara(PN))
-    
-print(NbNElmtTree(T2))
+        return NbNElmtTree(akar(PN)) + NbNElmtChild(saudara(PN))    
+
+
+# NbNDaun : PohonN-ner --> integer >= 0
+# {NbNDaun(PN) menghitung banyaknya daun pada pohon PN.}
+
+# REALISASI
+
 def NbNDaun(PN):
     if IsTreeEmpty(PN):
         return 0
@@ -73,10 +92,39 @@ def NbNDaun(PN):
     else:
         return NbNDaunChild(anak(PN))
 
+# NbNDaunChild : PohonN-ner --> integer >= 0
+# {NbNDaunChild(PN) menghitung jumlah daun pada sisa - sisa anak PN.}
+
+# REALISASI
+
 def NbNDaunChild(PN):
     if IsTreeEmpty(PN):
         return 0
     else:
         return NbNDaun(akar(PN)) + NbNDaunChild(saudara(PN)) 
     
-print(NbNDaun(T2))
+
+
+
+# APLIKASI
+
+print(IsTreeEmpty(makePN('A',[])))
+
+print(IsOneELmtTree(makePN('A',[makePN('B',[])])))
+
+print(NbNElmtTree(makePN('A',[makePN('B',[makePN('D',[]),makePN('E',[]),makePN('F',[])]),
+                 makePN('C',[makePN('G',[]),makePN('H',[makePN('I',[])])])])))
+
+print(NbNDaun(makePN('A',[makePN('B',[makePN('D',[]),makePN('E',[]),makePN('F',[])]),
+                 makePN('C',[makePN('G',[]),makePN('H',[makePN('I',[])])])])))
+
+
+
+
+
+
+
+
+
+
+
