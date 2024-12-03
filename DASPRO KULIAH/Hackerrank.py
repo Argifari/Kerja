42008ee02d3ade440574ed42dc7ed4bdabe9d324
Argifari <argifari40@gmail.com>
2024-12-03 22:30:55 +0700

    SSS

--- DASPRO KULIAH/Hackerrank.py
+++ DASPRO KULIAH/Hackerrank.py
@@ -1,78 +1,169 @@
-# from LOL import *
-# from list import *
+# Nama File: matematika_gura.py
+# Pembuat: 
+# Tanggal: 
+# Deskripsi: Mengembalikan list hasil tambah-tambah dua buah list sebagai representasi integer.
 
+# DEFINISI DAN SPESIFIKASI
+# shrimp --> list: integer
+# shrimp(X, Y) mengembalikan list hasil tambah-tambah dua buah list sebagai representasi integer.
 
-# type shelf: <tag: string,
-#               book: list of string>
-
-# koleksi objek
-# type shelves: <rakbuku: list of shelf>
-
-# NOTE: tag dan book adalah komponen dari shelf
-#       shelf adalah elemen dari shelves 
-
-# --------------- LIBRARY LoL AND List ------------------
-
-def MakeList(a):
-    return [a]
-
-def Konso(e, L):
+# REALISASI
+def konso(e, L):
     return [e] + L
-
-def Konsi(L, e):
+    
+def konsi(L, e):
     return L + [e]
-
-def concatList(L1, L2):
-    return L1 + L2
-
-def FirstElmt(L):
+  
+def first_elmt(L):
     return L[0]
-
-def LastElmt(L):
+  
+def last_elmt(L):
     return L[-1]
-
-def Head(L):
+  
+def head(L):
     return L[:-1]
-
-def Tail(L):
+  
+def tail(L):
     return L[1:]
 
-def IsEmpty(L):
-    return L == []
+def is_empty(L):
+    return len(L) == 0
 
-def FirstList(LoL):
-    return LoL[0]
+def IsOneElmt(L:list) -> bool:
+    return head(L) == [] and tail(L) == []
 
-def LastList(LoL):
-    return LoL[-1]
+def NbElmt(L:list) -> int:
+    if is_empty(L):
+        return 0
+    else :
+        return 1 + NbElmt(tail(L))
 
-def HeadList(LoL):
-    return LoL[:-1]
-
-def TailList(LoL):
-    return LoL[1:]
-
-def IsEmpty(LoL):
-    if LoL == []:
-        return True
+def Digit(X,Y):
+    if is_empty(X) or is_empty(Y):
+        return []
     else:
+        if NbElmt(X) > NbElmt(Y):
+            return Digit(X, konso(0,Y))
+        elif NbElmt(X) < NbElmt(Y):
+            return Digit(konso(0,X), Y)
+        else:
+            return [X] + Y
+
+def BesarMana(X,Y,tandaX,tandaY):
+    if is_empty(X):
+        return []
+    else:
+        if first_elmt(X) > first_elmt(Y):
+            if tandaX == '+':
+                return []
+            return [tandaX]
+        elif first_elmt(Y) > first_elmt(X):
+            if tandaY == '+':
+                return []
+            return [tandaY]
+        else:
+            return BesarMana(tail(X),tail(Y),tandaX,tandaY)
+
+def BesarX(X,Y):
+    if is_empty(X):
         return False
-
-def IsAtom(LoL):
-    if type(LoL) == list:
-             return False
     else:
-        return True
+        if first_elmt(X) > first_elmt(Y):
+            return True
+        elif first_elmt(Y) > first_elmt(X):
+            return False
+        else:
+            return BesarX(tail(X),tail(Y))
+        
+def kosong(a,b):
+    return a + b
 
-def IsList(LoL):
-    if IsAtom(LoL) == False:
-        return True
+def penghitung(X,Y,e,tandaX,tandaY,b,cek):
+    if is_empty(X):
+        if e == 1:
+            return [e]
+        else:
+            return []
     else:
-        return False
+        if tandaX == '-' and tandaY == '-' or (tandaX == '+' and tandaY == '+'):
+            if last_elmt(X) + last_elmt(Y) + e > 9:
+                return konsi(penghitung(head(X),head(Y),1,tandaX,tandaY,b,last_elmt(X) + last_elmt(Y) + e - 10), last_elmt(X) + last_elmt(Y) + e - 10)
+            else :
+                return konsi(penghitung(head(X),head(Y),0,tandaX,tandaY,b,(last_elmt(X) + last_elmt(Y) + e)), (last_elmt(X) + last_elmt(Y) + e))
+        elif tandaX == '-' and tandaY == '+' or (tandaX == '+' and tandaY == '-'):
+            if b:
+                if last_elmt(X) >= last_elmt(Y):
+                    return konsi(penghitung(head(X),head(Y),0,tandaX,tandaY,b,(last_elmt(X) - last_elmt(Y) + e) % 10), (last_elmt(X) - last_elmt(Y) + e) % 10)
+                else:
+                    return konsi(penghitung(head(X),head(Y),-1,tandaX,tandaY,b, 10 + last_elmt(X) - last_elmt(Y) + e), 10 + last_elmt(X) - last_elmt(Y) + e)
+                
+            else:
+                if last_elmt(Y) >= last_elmt(X):
+                    return konsi(penghitung(head(X),head(Y),0,tandaX,tandaY,b,(last_elmt(Y) - last_elmt(X) + e) % 10), (last_elmt(Y) - last_elmt(X) + e) % 10)
+                else:
+                    return konsi(penghitung(head(X),head(Y),-1,tandaX,tandaY,b,10 + last_elmt(Y) - last_elmt(X) + e), 10 + last_elmt(Y) - last_elmt(X) + e)
 
-# --------------- FUNCTION USED IN ADDBOOKS --------------
 
-# SELEKTOR
+
+def hapus(X):
+    if is_empty(X):
+        return []
+    elif first_elmt(X) != 0:
+        return X
+    else:
+        if first_elmt(X) == 0 and is_empty(tail(X)):
+            return X
+        else:
+            return hapus(tail(X))
+
+
+
+def shrimp(X, Y):
+    if is_empty(X) and is_empty(Y):
+        return []
+    elif is_empty(X) and not is_empty(Y):
+        return Y
+    elif is_empty(Y) and not is_empty(X):
+        return X
+    else:
+        if first_elmt(X) == '-' and first_elmt(Y) == '-':
+            return BesarMana(first_elmt(Digit(tail(X),tail(Y))),tail(Digit(tail(X),tail(Y))),'-','-') + penghitung(first_elmt(Digit(tail(X),tail(Y))),tail(Digit(tail(X),tail(Y))),0,'-','-'
+                             ,BesarX(first_elmt(Digit(tail(X),tail(Y))),tail(Digit(tail(X),tail(Y)))),last_elmt(X))
+        
+        elif first_elmt(X) == '-' and first_elmt(Y) != '-':
+            return BesarMana(first_elmt(Digit(tail(X),Y)),tail(Digit(tail(X),Y)),'-','+') + hapus(penghitung(first_elmt(Digit(tail(X),Y)),tail(Digit(tail(X),Y)),0,'-','+'
+                             ,BesarX(first_elmt(Digit(tail(X),Y)),tail(Digit(tail(X),Y))),last_elmt(X)))
+        
+        elif first_elmt(X) != '-' and first_elmt(Y) == '-':
+            return BesarMana(first_elmt(Digit(X,tail(Y))),tail(Digit(X,tail(Y))),'+','-') + hapus(penghitung(first_elmt(Digit(X,tail(Y))),tail(Digit(X,tail(Y))),0,'+','-'
+                              ,BesarX(first_elmt(Digit(X,tail(Y))),tail(Digit(X,tail(Y)))),last_elmt(X)))
+        
+        else:
+            return BesarMana(first_elmt(Digit(X,Y)),tail(Digit(X,Y)),'+','+') + penghitung(first_elmt(Digit(X,Y)),tail(Digit(X,Y)),0,'+','+'
+                            ,BesarX(first_elmt(Digit(X,Y)),tail(Digit(X,Y))),last_elmt(X))
+
+def GeserList(L):
+    if IsEmpty(L):
+        return []
+    else:
+        if IsAtom(FirstList(L)):
+            return KonsLo(FirstList(L), GeserList(TailList(L)))
+        elif IsList(FirstList(L)):
+            return KonsLi(GeserList(TailList(L)), FirstList(L))
+
+def EvaluateExpression(L):
+    if IsEmpty(L):
+        return []
+    else:
+        if operator(L) == '+':
+            return operand1(L) + operand2(L)
+        elif operator(L) == '-':
+            return operand1(L) - operand2(L)
+        elif operator(L) == '*':
+            return operand1(L) * operand2(L)
+        elif operator(L) =='/':
+            return (operand1(L) / operand2(L))
+ 
 def getTag(shelf):
     return FirstElmt(FirstList(shelf))
 
@@ -107,14 +198,4 @@ def AddBooks(shelves, tag, book):
             return makeShelf(getTag(shelves), getBooks(shelves) + book) + TailList(shelves)
         else:
             return Konso(FirstList(shelves),AddBooks(TailList(shelves),tag,book))
-
-
-
-# ----------------- APLIKASI ------------------------
-
-bo1 = AddBooks([['Novel', ['Toko Kelontong Namiya', 'Bumi', 'The Little Prince']], ['Komputer', ['Clean Code', 'Modern OS', 'Computer Network', 'DSA', 'Algorithms']], ['Biologi', ['Sobotta', 'The Selfish Gene', 'Kepunahan Keenam']], ['Fisika', ['A Brief History of Time', 'The Elegant Universe', 'Astrofisika untuk Orang Sibuk']]], "Komputer", ['Machine Learning', 'BCI'])
-bo2 = [['Novel', ['Toko Kelontong Namiya', 'Bumi', 'The Little Prince']], ['Komputer', ['Clean Code', 'Modern OS', 'Computer Network', 'DSA', 'Algorithms', 'Machine Learning', 'BCI']], ['Biologi', ['Sobotta', 'The Selfish Gene', 'Kepunahan Keenam']], ['Fisika', ['A Brief History of Time', 'The Elegant Universe', 'Astrofisika untuk Orang Sibuk']]]
-a = lambda a,b : True if a == b else False
-print(bo1)
-print(a(bo1,bo2))
-# SEMANGAT ^^
+print(shrimp(['-',1,3],[1,2]))
